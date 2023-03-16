5823f8106f4680f6b52e8f28d460adb990db190b
etiennewayne <etiennewayne@gmail.com>
2023-03-16 16:32:06 +0800

    update startup files

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,6 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id('user_id');
             $table->string('username')->unique();
-            $table->string('rfid')->nullable();
             $table->string('lname');
             $table->string('fname');
             $table->string('mname')->nullable();
